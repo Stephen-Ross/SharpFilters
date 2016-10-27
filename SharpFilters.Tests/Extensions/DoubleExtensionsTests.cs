7af8dd7761029627bdb1d121baeb3f9338ba3d56
stephenross <ross.stephen4@googlemail.com>
2016-10-27 21:24:25 +0100

    Added tests to cover the double extensions.

---
+++ SharpFilters.Tests/Extensions/DoubleExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using SharpFilters.Extensions;
+using SharpFilters.Tests.TestsCommon;
+using Xunit;
+
+namespace SharpFilters.Tests.Extensions
+{
+    public class DoubleExtensionsTests
+    {
+        [Theory]
+        [AutoMoqData]
+        internal void Subtract_SubtractsTheComplexFromTheValue_Test(
+            IEnumerable<Complex> complexs, double value)
+        {
+            var expected = complexs.Select(x => value - x);
+
+            Assert.Equal(expected, value.Subtract(complexs));
+        }
+    }
+}
