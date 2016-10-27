c397e754db0ec1786f478adc951f15b228c94454
stephenross <ross.stephen4@googlemail.com>
2016-10-27 22:05:38 +0100

    Added tests to cover the polynomial coefficients.

---
+++ SharpFilters.Tests/Models/PolynomialCoefficientsTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Ploeh.AutoFixture.Xunit2;
+using SharpFilters.Models;
+using SharpFilters.Tests.TestsCommon;
+using Xunit;
+
+namespace SharpFilters.Tests.Models
+{
+    public class PolynomialCoefficientsTests
+    {
+        [Theory]
+        [AutoMoqData]
+        internal void A_IsCorrectlySet_Test(
+            [Frozen(Matching.ParameterName)] IReadOnlyList<double> a,
+            PolynomialCoefficients polynomialCoefficients)
+        {
+            Assert.Equal(a, polynomialCoefficients.A);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        internal void B_IsCorrectlySet_Test(
+            [Frozen(Matching.ParameterName)] IReadOnlyList<double> b,
+            PolynomialCoefficients polynomialCoefficients)
+        {
+            Assert.Equal(b, polynomialCoefficients.B);
+        }
+    }
+}
