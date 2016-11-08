28985906d13b4a3731dfe5c6d0b2ad2981a6e653
stephenross <ross.stephen4@googlemail.com>
2016-11-08 22:03:01 +0000

    Added thread synchronisation to the continuous filter as there was the ability to get into an inconsistent state.

--- SharpFilters/ContinuousFilter.cs
+++ SharpFilters/ContinuousFilter.cs
@@ -1,7 +1,12 @@
-﻿namespace SharpFilters
+﻿using System;
+using System.Threading;
+
+namespace SharpFilters
 {
     internal class ContinuousFilter : IContinuousFilter
     {
+        private static SpinLock spinLock = new SpinLock();
+
         private readonly IFilterDesign filterDesign;
 
         private double[] xv;
@@ -17,36 +22,64 @@
 
         public double Filter(double data)
         {
-            for (var i = 0; i < this.xv.Length - 1; i++)
+            var lockTaken = false;
+
+            try
             {
-                this.xv[i] = this.xv[i + 1];
-                this.yv[i] = this.yv[i + 1];
+                spinLock.Enter(ref lockTaken);
+
+                for (var i = 0; i < this.xv.Length - 1; i++)
+                {
+                    this.xv[i] = this.xv[i + 1];
+                    this.yv[i] = this.yv[i + 1];
+                }
+
+                this.xv[xv.Length - 1] = data;
+
+                var filteredData = 0.0d;
+                var index = this.xv.Length - 1;
+                for (var i = 0; i < this.xv.Length; i++, index--)
+                {
+                    filteredData += this.filterDesign.PolynomialCoefficients.B[i] * this.xv[index];
+                }
+
+                index = this.yv.Length - 2;
+                for (var i = 0; i < this.yv.Length; i++, index--)
+                {
+                    filteredData -= this.filterDesign.PolynomialCoefficients.A[i] * this.yv[index];
+                }
+
+                this.yv[this.filterDesign.PolynomialCoefficients.A.Count - 1] = filteredData;
+
+                return filteredData;
             }
-
-            this.xv[xv.Length - 1] = data;
-
-            var filteredData = 0.0d;
-            var index = this.xv.Length - 1;
-            for (var i = 0; i < this.xv.Length; i++, index--)
+            finally
             {
-                filteredData += this.filterDesign.PolynomialCoefficients.B[i] * this.xv[index];
+                if (lockTaken)
+                {
+                    spinLock.Exit();
+                }
             }
-
-            index = this.yv.Length - 2;
-            for (var i = 0; i < this.yv.Length; i++, index--)
-            {
-                filteredData -= this.filterDesign.PolynomialCoefficients.A[i] * this.yv[index];
-            }
-
-            this.yv[this.filterDesign.PolynomialCoefficients.A.Count - 1] = filteredData;
-
-            return filteredData;
         }
 
         public void Reset()
         {
-            this.xv = new double[filterDesign.PolynomialCoefficients.B.Count];
-            this.yv = new double[filterDesign.PolynomialCoefficients.A.Count];
+            var lockTaken = false;
+
+            try
+            {
+                spinLock.Enter(ref lockTaken);
+
+                this.xv = new double[filterDesign.PolynomialCoefficients.B.Count];
+                this.yv = new double[filterDesign.PolynomialCoefficients.A.Count];
+            }
+            finally
+            {
+                if (lockTaken)
+                {
+                    spinLock.Exit();
+                }
+            }
         }
     }
 }
