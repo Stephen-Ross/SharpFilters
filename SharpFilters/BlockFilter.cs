4716820d568ecf20177f20bbe35702c5b2a9144c
stephenross <ross.stephen4@googlemail.com>
2016-11-13 18:42:43 +0000

    Refactored the block filter so that it uses the continious filter instead of requiring the filtering code.

--- SharpFilters/BlockFilter.cs
+++ SharpFilters/BlockFilter.cs
@@ -15,34 +15,11 @@ namespace SharpFilters
         {
             var filteredData = new List<double>(data.Count);
 
-            var xv = new double[filterDesign.PolynomialCoefficients.B.Count];
-            var yv = new double[filterDesign.PolynomialCoefficients.A.Count];
+            var continiousFilter = new ContinuousFilter(this.filterDesign);
 
             for (var i = 0; i < data.Count; i++)
             {
-                for (var j = 0; j < xv.Length - 1; j++)
-                {
-                    xv[j] = xv[j + 1];
-                    yv[j] = yv[j + 1];
-                }
-
-                xv[xv.Length - 1] = data[i];
-
-                var filteredValue = 0.0d;
-                var index = xv.Length - 1;
-                for (var j = 0; j < xv.Length; j++, index--)
-                {
-                    filteredValue += this.filterDesign.PolynomialCoefficients.B[j] * xv[index];
-                }
-
-                index = yv.Length - 2;
-                for (var j = 0; j < yv.Length; j++, index--)
-                {
-                    filteredValue -= this.filterDesign.PolynomialCoefficients.A[j] * yv[index];
-                }
-
-                yv[this.filterDesign.PolynomialCoefficients.A.Count - 1] = filteredValue;
-                filteredData.Add(filteredValue);
+                filteredData.Add(continiousFilter.Filter(data[i]));
             }
 
             return filteredData;
