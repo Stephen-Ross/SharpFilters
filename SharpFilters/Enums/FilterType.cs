42a6182d79cea888085b85641712acd469556aae
stephenross <ross.stephen4@googlemail.com>
2016-11-27 11:22:35 +0000

    Added the documentation to the filter type enum.

--- SharpFilters/Enums/FilterType.cs
+++ SharpFilters/Enums/FilterType.cs
@@ -2,10 +2,19 @@
 
 namespace SharpFilters.Enums
 {
+    /// <summary>
+    /// Represents the type of filter to be built.
+    /// </summary>
     public enum FilterType
     {
+        /// <summary>
+        /// A lowpass filter.
+        /// </summary>
         Lowpass,
 
+        /// <summary>
+        /// A highpass filter.
+        /// </summary>
         Highpass
     }
 }
